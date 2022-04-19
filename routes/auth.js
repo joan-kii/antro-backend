8c4ed9e06087ad7a8d2a0e07ed604f17a234befa
joan-kii <joan.sb81@gmail.com>
2022-04-19 17:53:28 +0200

    modifica wakeupserver

--- routes/auth.js
+++ routes/auth.js
@@ -7,14 +7,12 @@ import isAuthenticated from '../middleware/isAuthenticated.js';
 const router = express.Router();
 
 // Wake up server
-router.get('/wakeup', async (req, res, next) => {
-  if (process.env._.indexOf("heroku") !== -1) {
-    res.status(200).json({
-      success: true,
-      message: 'Ready!', 
-      payload: null
-    });
-  }
+router.get('/wakeup', (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: 'Ready!', 
+    payload: null
+  });
 });
 
 // User Log In
