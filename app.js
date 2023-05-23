66960f95671df02fcd86d4cf0e00213f1106f6f7
joan-kii <joan.sb81@gmail.com>
2023-05-23 05:15:58 +0200

    pruebas

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  console.log(err.message);
+  console.log(err.message, 'lol');
   res.status(err.status);
   res.json(err);
 });
